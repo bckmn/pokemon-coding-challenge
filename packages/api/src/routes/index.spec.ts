b345a35103174f6cc56e577f6d184c7c233d8df9
Simon Bäckman <simon@bckmn.se>
2024-12-06 10:59:13 +0100

    Replace jest with mocha

--- packages/api/src/routes/index.spec.ts
+++ packages/api/src/routes/index.spec.ts
@@ -1,35 +1,25 @@
-import express, { Express } from "express";
+import { expect } from "chai";
+import express from "express";
 import request from "supertest";
 
-import { pokemon } from "../assets/pokemon.js";
 import routes from "../routes/index.js";
 
 describe("Pokemon API", () => {
-  let app: Express;
+  const app = express();
+  app.use(routes);
 
-  beforeEach(() => {
-    app = express();
-    app.use(express.json());
-    app.use(routes);
+  it("GET / returns 404", async () => {
+    await request(app)
+      .get("/")
+      .expect(404);
   });
 
-  describe("GET /", () => {
-    it("should return 404", async () => {
-      await request(app)
-        .get("/")
-        .expect(404);
-    });
-  });
+  it("GET /pokemon returns all pokemon", async () => {
+    const response = await request(app)
+      .get("/pokemon")
+      .expect("Content-Type", /json/)
+      .expect(200);
 
-  describe("GET /pokemon", () => {
-    it("should return all pokemon", async () => {
-      const response = await request(app)
-        .get("/pokemon")
-        .expect("Content-Type", /json/)
-        .expect(200);
-
-      expect(response.body).toEqual({ pokemon });
-      expect(response.body.pokemon).toHaveLength(151);
-    });
+    expect(response.body.pokemon).to.have.lengthOf(151);
   });
 });
