2ca5e8f4ca0136782a1f15f6b4dcb9c03c33390f
Simon Bäckman <simon@bckmn.se>
2024-12-06 10:41:14 +0100

    Fix lint errors

--- packages/api/src/routes/index.spec.ts
+++ packages/api/src/routes/index.spec.ts
@@ -1,9 +1,10 @@
-import request from 'supertest';
-import express, { Express } from 'express';
-import routes from '../routes/index.js';
-import { pokemon } from '../assets/pokemon.js';
+import express, { Express } from "express";
+import request from "supertest";
 
-describe('Pokemon API', () => {
+import { pokemon } from "../assets/pokemon.js";
+import routes from "../routes/index.js";
+
+describe("Pokemon API", () => {
   let app: Express;
 
   beforeEach(() => {
@@ -14,17 +15,17 @@ describe('Pokemon API', () => {
 
   describe("GET /", () => {
     it("should return 404", async () => {
-        await request(app)
-            .get("/")
-            .expect(404);
+      await request(app)
+        .get("/")
+        .expect(404);
     });
-  })
+  });
 
-  describe('GET /pokemon', () => {
-    it('should return all pokemon', async () => {
+  describe("GET /pokemon", () => {
+    it("should return all pokemon", async () => {
       const response = await request(app)
-        .get('/pokemon')
-        .expect('Content-Type', /json/)
+        .get("/pokemon")
+        .expect("Content-Type", /json/)
         .expect(200);
 
       expect(response.body).toEqual({ pokemon });
